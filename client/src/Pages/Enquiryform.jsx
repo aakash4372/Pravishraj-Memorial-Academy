ff76497574172a95bf1bb62e4fd98d5ed01a2f17
aakash4372 <aakashaakash0013@gmail.com>
2025-08-07 09:15:27 +0530

    updated

--- client/src/Pages/Enquiryform.jsx
+++ client/src/Pages/Enquiryform.jsx
@@ -155,7 +155,7 @@ export function EnquiryFormModal({ isOpen, onOpenChange }) {
     console.log("Form submitted:", formData);
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://pravishraj-memorial-academy.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
